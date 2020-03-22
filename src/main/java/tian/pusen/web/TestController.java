15d5d553bd39cfe06d5a7844c325710333f3eb4d
pustian <pustian@msn.com>
2020-03-22 13:16:17 +0800

    基本的springboot, 包含profile和Junit

---
+++ src/main/java/tian/pusen/web/TestController.java
@@ -0,0 +1,13 @@
+package tian.pusen.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "hello world";
+    }
+}
